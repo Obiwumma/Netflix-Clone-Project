da0636629ff9783175fe5a7d261cbaf4a2dcd38a
Obiwumma Pascal <pobiwumma@gmail.com>
2025-04-20 19:59:52 +0100

    Implemented the plus and X display

--- script.js
+++ script.js
@@ -29,3 +29,90 @@ prevButton.addEventListener("click", () => {
 window.addEventListener("resize", () => {
   updateSliderPosition();
 });
+
+
+// FAQ section
+  
+const minus = document.querySelectorAll(".minus-sign")
+const plus = document.querySelectorAll(".plus-sign")
+const question = document.querySelectorAll(".question")
+const answer = document.querySelectorAll(".answer")
+
+// question.forEach(element => {
+//   element.addEventListener("click", () => {
+//     answer.forEach(ans => ans.style.display = "none")
+//     if (element == question[0]) {
+//       const isVisible = answer[0].style.display === "block";
+//       if(!isVisible) {
+//         answer[0].style.display = "block"
+//       } else {
+//         answer[0].style.display = "none"
+//       }
+      
+//     } else if (element == question[1]) {
+//       const isVisible = answer[1].style.display === "block";
+//       answer[1].style.display = isVisible ? "none" : "block";
+     
+//     } else if (element == question[2]) {
+//       const isVisible = answer[2].style.display === "block";
+//       answer[2].style.display = isVisible ? "none" : "block";
+     
+//     } else if (element == question[3]) {
+//       const isVisible = answer[3].style.display === "block";
+//       answer[3].style.display = isVisible ? "none" : "block";
+      
+//     } else if (element == question[4]) {
+//       const isVisible = answer[4].style.display === "block";
+//       answer[4].style.display = isVisible ? "none" : "block";
+      
+//     } else if (element == question[5]) {
+//       const isVisible = answer[5].style.display === "block";
+//       answer[5].style.display = isVisible ? "none" : "block";
+     
+//     } 
+//   })
+// });
+
+question.forEach((element, index) => {
+  element.addEventListener("click", () => {
+    const isVisible = answer[index].style.display === "block"; 
+
+    const plusVisible = plus[index].style.display === "none"
+    const minVisible = minus[index].style.display === "block"
+    
+    if (!plusVisible) {  
+      plus[index].style.display = "none"
+    } else {
+      plus[index].style.display = "block"
+    }
+
+    if (!minVisible) {
+      minus[index].style.display = "block"
+    } else {
+      minus[index].style.display = "none"
+    }   
+    answer.forEach(ans => ans.style.display = "none");
+
+    if (!isVisible) {
+      answer[index].style.display = "block";
+    }
+
+    
+    
+  });
+});
+
+// plus.forEach((element) => {
+//     element.style.display = "none"
+
+//     console.log(element.style.display)
+  // })
+
+
+
+  // const wasVisible = minus[index].style.display === "block"
+  // minus.forEach(min => min.style.display = "none")
+  
+  // if (!wasVisible) {
+  //   minus[index].style.display = "block"
+  // }
